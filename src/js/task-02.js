a1d0fac887f61c1143f93b1c49c316dcbeb172a5
domanskayelyzaveta <domanskaya_@ukr.net>
2023-08-26 15:49:52 +0200

    task 2 and 3 push

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,17 @@
+// HTML містить порожній список ul#ingredients.
+
+// <ul id="ingredients"></ul>
+
+// JavaScript містить масив рядків.
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +20,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+function task2() {
+
+  const findUl = document.querySelector("#ingredients");
+  console.log(findUl);
+
+  
+  ingredients.forEach(ingredients => {
+    const li = document.createElement("li");
+    li.textContent = ingredients;
+    li.classList.add("item");
+    findUl.append(li);
+  });
+};
+task2();
+
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,3 +1,18 @@
+
+
+
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список
+// ul.gallery.
+
+// <ul class="gallery"></ul>
+
+//   Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. 
+// Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +27,21 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+function task3() {
+
+  const findUl = document.querySelector(".gallery");
+
+  images.forEach((images) => {
+    const li = document.createElement("li");
+    console.log(li);
+    const image = document.createElement("img");
+    image.src = images.url;
+    image.alt = images.alt;
+    findUl.append(li);
+    li.append(image);
+    image.insertAdjacentHTML("beforeend", images.alt);
+  });
+}
+task3();
