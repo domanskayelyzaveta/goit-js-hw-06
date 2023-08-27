5069afcebdc76010946bd76550944538b13a0b3a
domanskayelyzaveta <130005425+domanskayelyzaveta@users.noreply.github.com>
2023-08-27 17:54:03 +0000

    Deploying to gh-pages from @ domanskayelyzaveta/goit-js-hw-06@e05d6ccbb23c27677f0841178f9414b32df7345e 🚀

---
+++ task-04.604e0c3b.js
@@ -0,0 +1,2 @@
+let e=0;const t=document.querySelector("#value"),n=document.querySelector("#counter"),c=n.firstElementChild,l=n.lastElementChild;function o(n){"increment"===n.target.dataset.action?e+=1:e-=1,console.log(e),t.textContent=e}c.addEventListener("click",o),l.addEventListener("click",o);
+//# sourceMappingURL=task-04.604e0c3b.js.map
