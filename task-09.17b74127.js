8dd1ce1ef6b51afbf31eabaa644e84b3f5dfbc35
domanskayelyzaveta <130005425+domanskayelyzaveta@users.noreply.github.com>
2023-08-28 15:24:41 +0000

    Deploying to gh-pages from @ domanskayelyzaveta/goit-js-hw-06@d8cf0228ab2e6ba959bae43e1bf7359d060d6625 🚀

---
+++ task-09.17b74127.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector(".change-color");let o=document.querySelector(".color");e.addEventListener("click",(function(e){let r=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=r,o.textContent=r}));
+//# sourceMappingURL=task-09.17b74127.js.map
---
+++ task-09.99573258.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("body"),t=document.querySelector(".change-color"),e=document.querySelector(".color");t.addEventListener("click",(function(t){var r="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));o.style.backgroundColor=r,e.textContent=r}))}();
+//# sourceMappingURL=task-09.99573258.js.map
